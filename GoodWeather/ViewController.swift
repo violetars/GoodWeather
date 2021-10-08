395e4bae2bba989b957db0da38bbd656526250ae
Violeta Recio <violeta.recio@kairosds.com>
2021-10-08 14:49:12 +0200

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Violeta Recio Sansón on 8/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
